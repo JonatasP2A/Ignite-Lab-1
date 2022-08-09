b44356d4d8dff21479c45c5bf8c17d578374c25d
Jonatas Alves <jonatasalves@id.uff.br>
2022-08-08 22:30:48 -0300

    withApollo hook

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,15 +1,29 @@
+import { gql, useQuery } from '@apollo/client';
 import {
   getAccessToken,
   useUser,
   withPageAuthRequired,
 } from '@auth0/nextjs-auth0';
+import { withApollo } from '../../lib/withApollo';
 
-export default function Home() {
+const PRODUCTS_QUERY = gql`
+  query GetProducts {
+    products {
+      id
+      title
+    }
+  }
+`;
+
+function Home() {
   const { user } = useUser();
+  const { data, loading, error } = useQuery(PRODUCTS_QUERY);
 
   return (
     <div>
       <h1>Hello World</h1>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
 
       <pre>{JSON.stringify(user, null, 2)}</pre>
     </div>
@@ -25,3 +39,5 @@ export const getServerSideProps = withPageAuthRequired({
     };
   },
 });
+
+export default withApollo(Home);
